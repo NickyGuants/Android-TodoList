aadb2d5ee86588f2ccec80f75f2a0cba9a247931
NickyGuants <nicholasguantai528@gmail.com>
2021-05-21 10:23:38 +0300

    Create the adapter for the recycler view

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -1,24 +1,44 @@
 package com.example.todoapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.todoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var todoAdapter: TodoAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
+        setContentView(R.layout.activity_main)
         setSupportActionBar(binding.toolbar)
+        todoAdapter = TodoAdapter(mutableListOf())
 
+        val recyclerView =findViewById<RecyclerView>(R.id.recycle_view)
+        recyclerView.adapter = todoAdapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
+        val addBtn = findViewById<Button>(R.id.button)
+        val edit = findViewById<EditText>(R.id.edit_text)
+        addBtn.setOnClickListener{
+            val todoTitle = edit.text.toString()
+            if(todoTitle.isNotEmpty()){
+                val todo = Todo(todoTitle)
+                todoAdapter.addTodo(todo)
+                edit.text.clear()
+            }
+        }
+
+        val deleteButton= findViewById<Button>(R.id.delete_button)
+        deleteButton.setOnClickListener{
+            todoAdapter.deleteDoneTodos()
+        }
 
     }
-
-
 }
